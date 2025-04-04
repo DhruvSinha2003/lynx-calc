7e1a6d10dc3ad6c8d04bf2f4c47b5e582da72efa
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-04-05 00:50:18 +0530

    increment working

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,24 @@
 import { useCallback, useEffect, useState } from "@lynx-js/react";
 
 import "./App.css";
-import arrow from "./assets/arrow.png";
-import lynxLogo from "./assets/lynx-logo.png";
-import reactLynxLogo from "./assets/react-logo.png";
 
 export function App() {
-  const [alterLogo, setAlterLogo] = useState(false);
+  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    console.info("Hello, ReactLynx");
-  }, []);
-
-  const onTap = useCallback(() => {
-    "background only";
-    setAlterLogo(!alterLogo);
-  }, [alterLogo]);
+  const onTap = () => {
+    setCount(count + 1);
+  };
 
   return (
     <view>
       <view className="Background" />
       <view className="App">
-        <view className="Banner">
-          <view className="Logo" bindtap={onTap}>
-            {alterLogo ? (
-              <image src={reactLynxLogo} className="Logo--react" />
-            ) : (
-              <image src={lynxLogo} className="Logo--lynx" />
-            )}
-          </view>
-          <text className="Title">React</text>
-          <text className="Subtitle">on Lynx</text>
-        </view>
-        <view className="Content">
-          <image src={arrow} className="Arrow" />
-          <text className="Description">Tap the logo and have fun!</text>
-          <text className="Hint">
-            Edit<text style={{ fontStyle: "italic" }}>{" src/App.tsx "}</text>
-            to see updates!
+        <view className="Count">
+          <text className="Count-Text">{count}</text>
+          <text className="Count-Text" bindtap={onTap}>
+            Increment
           </text>
         </view>
-        <view style={{ flex: 1 }}></view>
       </view>
     </view>
   );
